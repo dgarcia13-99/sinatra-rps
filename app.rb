5e56f640ac7f0ee14fc085ae244133d11f3313e8
Daniela Garcia <danielagarcia13.99@gmail.com>
2024-09-08 05:02:57 +0000

    created the table and added all images and links to the home page

--- app.rb
+++ app.rb
@@ -6,9 +6,26 @@ get("/") do
 end
 
 get("/rock") do
+
   my_pick="rock"
   options=["rock", "paper", "scissors"]
   @player_pick=options.sample
   
   erb(:rock)
+end
+
+get("/paper") do
+  my_pick="paper"
+  options=["rock", "paper", "scissors"]
+  @player_pick=options.sample
+  
+  erb(:paper)
+end
+
+get("/scissors") do
+  my_pick="scissors"
+  options=["rock", "paper", "scissors"]
+  @player_pick=options.sample
+  
+  erb(:scissors)
 end
